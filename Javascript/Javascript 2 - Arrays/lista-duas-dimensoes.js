236460fc4c202fa0657231d17e7f62020755a149
vitorsantini <santini.vitor@hotmail.com>
2023-05-16 00:23:37 -0300

    Criando Pasta do Curso de Javascript: Arrays

---
+++ Javascript/Javascript 2 - Arrays/lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`a aluna da posição 1 na lista de alunos é ${listaDeAlunosEMedias [0][1]}, a nota dessa aluna é ${listaDeAlunosEMedias[1][1]}.`);
