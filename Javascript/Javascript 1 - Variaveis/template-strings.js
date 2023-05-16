be3e261873d34c72d2338b430d485baf04f46d62
vitorsantini <santini.vitor@hotmail.com>
2023-05-16 00:22:45 -0300

    Criando Pasta do Curso de Javascript: tipos, variáveis e funções

---
+++ Javascript/Javascript 1 - Variaveis/template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Vitor";
+const idade = 2023-2021;
+const cidadeDeNascimento = "São José do Rio Preto";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
+
+
