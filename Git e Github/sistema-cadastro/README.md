b8a01da85583c69120bd3e3204ffe3996d053136
vitorsantini <santini.vitor@hotmail.com>
2023-05-23 00:26:06 -0300

    adicionando sistema-cadastro e nodejs

---
+++ Git e Github/sistema-cadastro/README.md
@@ -0,0 +1,13 @@
+<h1> Sistema de Cadastro de jogos </h1>
+
+> Status do projeto: Em Desenvolvimento
+
+Para rodar esse projeto na sua maquina, por favor digite:
+
+```
+
+node app.js
+
+```
+ :) 
+ 
---
+++ NodeJS/src/cli.js
@@ -0,0 +1,46 @@
+import chalk from "chalk";
+import fs from 'fs';
+import pegaArquivo from "./index.js";
+import listaValidada from "./http-validação.js";
+
+const caminho = process.argv;
+
+async function imprimeLista(valida, resultado, identificador = '') {
+    if (valida){
+        console.log(
+            chalk.yellow('Lista validada:'),
+            chalk.black.bgGreen(identificador),
+            await listaValidada(resultado));
+    } else {
+        console.log(
+            chalk.yellow('Lista de links:'),
+            chalk.black.bgGreen(identificador),
+            resultado);
+    }
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
+    try{
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if(erro.code === 'ENOENT'){
+            console.log('Arquivo ou diretorio não existente!')
+            return;
+        }
+    }
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        imprimeLista(valida, resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+            imprimeLista(valida, lista, nomeDeArquivo);
+        })
+    }
+}
+
+processaTexto(caminho);
