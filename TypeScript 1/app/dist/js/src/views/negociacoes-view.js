4e13ed622b3641f4258d6386aedd91750a178c48
vitorsantini <santini.vitor@hotmail.com>
2023-06-29 23:08:02 -0300

    Adicionando Projeto TypeScript

---
+++ TypeScript 1/app/dist/js/src/views/negociacoes-view.js
@@ -0,0 +1,31 @@
+import { View } from "./view.js";
+export class NegociacoesView extends View {
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+            <thread>
+                <tr>
+                    <th>Data</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                </tr>
+            </thread>
+            <tbody>
+                ${model.lista().map(negociacao => {
+            return `
+                        <tr>
+                            <td>${this.formatar(negociacao.data)}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                        </tr>
+                    `;
+        }).join('')}
+            </tbody>
+        </table>
+
+        `;
+    }
+    formatar(data) {
+        return new Intl.DateTimeFormat().format(data);
+    }
+}
